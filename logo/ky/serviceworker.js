98b0dc3fe8df90f4d37042ace3dd64590195d01a
Rifky <rifky060796@gmail.com>
2020-03-28 23:26:20 +0700

    Update serviceworker.js

--- logo/ky/serviceworker.js
+++ logo/ky/serviceworker.js
@@ -1,108 +1,110 @@
 (function() {
-  var nativeAddAll = Cache.prototype.addAll;
-  var userAgent = navigator.userAgent.match(/(Firefox|Chrome)/(d+.)/);
+    var nativeAddAll = Cache.prototype.addAll;
+    var userAgent = navigator.userAgent.match(/(Firefox|Chrome)/(d + .) / );
 
-  
-  if (userAgent) {
-    var agent = userAgent[1];
-    var version = parseInt(userAgent[2]);
-  }
 
-  if (
-    nativeAddAll && (!userAgent ||
-      (agent === 'Firefox' && version >= 46) ||
-      (agent === 'Chrome'  && version >= 50)
-    )
-  ) {
-    return;
-  }
-
-  Cache.prototype.addAll = function addAll(requests) {
-    var cache = this;
-
-    
-    function NetworkError(message) {
-      this.name = 'NetworkError';
-      this.code = 19;
-      this.message = message;
+    if (userAgent) {
+        var agent = userAgent[1];
+        var version = parseInt(userAgent[2]);
     }
 
-    NetworkError.prototype = Object.create(Error.prototype);
+    if (
+        nativeAddAll && (!userAgent ||
+            (agent === 'Firefox' && version >= 46) ||
+            (agent === 'Chrome' && version >= 50)
+        )
+    ) {
+        return;
+    }
 
-    return Promise.resolve().then(function() {
-      if (arguments.length < 1) throw new TypeError();
+    Cache.prototype.addAll = function addAll(requests) {
+        var cache = this;
 
-      
-      var sequence = [];
 
-      requests = requests.map(function(request) {
-        if (request instanceof Request) {
-          return request;
+        function NetworkError(message) {
+            this.name = 'NetworkError';
+            this.code = 19;
+            this.message = message;
         }
-        else {
-          return String(request); 
-        }
-      });
 
-      return Promise.all(
-        requests.map(function(request) {
-          if (typeof request === 'string') {
-            request = new Request(request);
-          }
+        NetworkError.prototype = Object.create(Error.prototype);
 
-          var scheme = new URL(request.url).protocol;
+        return Promise.resolve().then(function() {
+            if (arguments.length < 1) throw new TypeError();
 
-          if (scheme !== 'http:' && scheme !== 'https:') {
-            throw new NetworkError("Invalid scheme");
-          }
 
-          return fetch(request.clone());
-        })
-      );
-    }).then(function(responses) {
-      // If some of the responses has not OK-eish status,
-      // then whole operation should reject
-      if (responses.some(function(response) {
-        return !response.ok;
-      })) {
-        throw new NetworkError('Incorrect response status');
-      }
+            var sequence = [];
 
-      
-      return Promise.all(
-        responses.map(function(response, i) {
-          return cache.put(requests[i], response);
-        })
-      );
-    }).then(function() {
-      return undefined;
-    });
-  };
+            requests = requests.map(function(request) {
+                if (request instanceof Request) {
+                    return request;
+                } else {
+                    return String(request);
+                }
+            });
 
-  Cache.prototype.add = function add(request) {
-    return this.addAll([request]);
-  };
+            return Promise.all(
+                requests.map(function(request) {
+                    if (typeof request === 'string') {
+                        request = new Request(request);
+                    }
+
+                    var scheme = new URL(request.url).protocol;
+
+                    if (scheme !== 'http:' && scheme !== 'https:') {
+                        throw new NetworkError("Invalid scheme");
+                    }
+
+                    return fetch(request.clone());
+                })
+            );
+        }).then(function(responses) {
+            // If some of the responses has not OK-eish status,
+            // then whole operation should reject
+            if (responses.some(function(response) {
+                    return !response.ok;
+                })) {
+                throw new NetworkError('Incorrect response status');
+            }
+
+
+            return Promise.all(
+                responses.map(function(response, i) {
+                    return cache.put(requests[i], response);
+                })
+            );
+        }).then(function() {
+            return undefined;
+        });
+    };
+
+    Cache.prototype.add = function add(request) {
+        return this.addAll([request]);
+    };
 }());
 
 self.addEventListener('install', function(e) {
-e.waitUntil(
-caches.open('iskyblog').then(function(cache) {
-return cache.addAll([
-'/'
-]);
-})
-);
+    e.waitUntil(
+        caches.open('iskyblog').then(function(cache) {
+            return cache.addAll([
+                '/'
+            ]);
+        })
+    );
 });
 self.addEventListener('fetch', function(event) {
-console.log(event.request.url);
-event.respondWith(
-caches.match(event.request).then(function(response) {
-return response || fetch(event.request);
-})
-);
+    console.log(event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
 
-    if('serviceWorker' in navigator) {
-  navigator.serviceWorker
-           .then(function() { console.log("Service Worker Registered"); });
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/serviceworker.js')
+        .then(function() {
+            console.log("Service Worker Registered");
+        });
 }
